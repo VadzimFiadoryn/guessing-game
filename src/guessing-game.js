722742b9c25cc487b528e0c4c84821153ea85c34
vadzimfiadoryn <vadzimfiadoryn@gmail.com>
2019-10-12 22:14:46 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        var answer = 0;
+        var left = 0;
+        var right = 0;
+    }
 
     setRange(min, max) {
-
+        this.left = min;
+        this.right = max;
     }
 
     guess() {
-
+        return this.answer = Math.round((this.right + this.left) / 2);
     }
 
     lower() {
-
+        return this.right = this.answer;
     }
 
     greater() {
-
+        return this.left = this.answer;
     }
 }
-
 module.exports = GuessingGame;
